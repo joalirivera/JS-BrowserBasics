30aee46e79a731b62d1f782694d68b5e237645d1
wallyyworld <rjoali@ymail.com>
2016-08-13 20:45:42 +0000

    JSBasics2

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 
-alert('Hello Boulder!');
+//alert('Hello Boulder!');
+
+var greeting = "What is your name?"; 
+
+function getName() {
+    return prompt(greeting);
+}
+
+function yourFunction() {
+     alert("Hello "+getName());
+}
+
+yourFunction();
